06402097d49f800424c23c9da5421bbbf7836bac
andrijagrujic <andrija.grujic.98@gmail.com>
2020-06-19 17:59:49 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+        <link href="css/main.css" rel="stylesheet">
+        <link href="fonts/roboto/Roboto-Regular.ttf" rel="stylesheet">
+<!--        <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap" rel="stylesheet">-->
+<!--        <link href="https://fonts.googleapis.com/css2?family=Syncopate&display=swap" rel="stylesheet">-->
+
+        <title>Bralo Group</title>
+    </head>
+
+    <body id="font" class="container-fluid">
+        <div class="content-wrapper">
+            <div class="header-logo-wrapper pt-5 pl-5">
+                <div class="header-container w-full">
+                    <div class="max-w-xs">
+                        <span class="bralo-group-heading block text-right">BRALO GROUP</span>
+                    </div>
+                </div>
+
+                <img class="object-contain mx-auto w-1/4"
+                    src="images/logo_no_name.jpg"
+                    alt="Bralo Group">
+            </div>
+            <div class="text-copyright-wrapper pl-8">
+                <h1 class="real-estate-text text-center text-4xl py-4">REAL ESTATE SINCE 1996</h1>
+                <p class="pl-2 pb-4">Copyright © Bralo group 1996 - 2020</p>
+            </div>
+        </div>
+
+        <div class="flex mb-2 border-top border-black h-64">
+            <div class="w-1/5 bg-gray-100 text-center border-r-2"></div>
+            <div class="w-1/5 bg-gray-100 text-center border-r-2"></div>
+            <div class="w-1/5 bg-gray-100 text-center border-r-2"></div>
+            <div class="w-1/5 bg-gray-100 text-center border-r-2"></div>
+            <div class="w-1/5 bg-gray-100 text-center"></div>
+        </div>
+
+        <footer>
+            <ul class="flex m-2">
+                <li class="w-1/6 text-center">Kalischer Straße 24</li>
+                <li class="w-1/6 text-center list-disc">10713 Berlin</li>
+                <li class="w-1/6 text-center list-disc">Deutschland</li>
+                <li class="w-1/6 text-center list-disc">Tel - 030 / 810 95 800</li>
+                <li class="w-1/6 text-center list-disc">Fax - 030 / 810 95 801</li>
+                <li class="w-1/6 text-center list-disc"><a href="mailto:info@example.com">info@bralo.group</a></li>
+            </ul>
+        </footer>
+    
+    </body>
+</html>
